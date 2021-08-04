dc9fca68ea0642a342acf6d71edfc1c19527b12e
Tariqkumraj <86144917+Tariqkumraj@users.noreply.github.com>
2021-08-04 14:52:24 +0500

    Add files via upload

--- auth.js
+++ auth.js
@@ -1,11 +1,31 @@
 const express = require ('express');
-const { signup, signin, requireSignin } = require('../../controller/admin/auth');
+const { signup, signin, requireSignin } = require('../controller/auth');
+const { check } = require('express.validator');
 const router = express.Router();
 
 
-router.post('/admin/signup', signup);
+router.post('/signup', [
+    check('firstName')
+    .notEmpty()
+    .withMessage('firstName is required'),
+    check('lastName')
+    .notEmpty()
+    .withMessage('lastName is required'),
+    check('lastName'),
+    check('email')
+    .isEmail()
+    .withMessage('Email is required'),
+    check('password')
+    .isLength({min: 6 })
+    .withMessage('password must be at least 6 character long')
 
-router.post('/admin/signin', signin);
+], signup);
 
 
+router.post('/signin', signin);
+/*
+router.post('/profile', requireSignin, (req, res) => {
+ res.status(200).json({ user: 'profile'})
+});
+*/
 module.exports = router;
